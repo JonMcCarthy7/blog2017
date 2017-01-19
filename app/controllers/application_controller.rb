f698e2d54b2ff2f8f30548dcaad0374a9dbf5091
JonMcCarthy7 <jtmcc93@gmail.com>
2017-01-19 12:34:49 -0700

    add navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,10 +12,10 @@ before_action :configure_permitted_parameters, if: :devise_controller?
  protected
 
  def configure_permitted_parameters
-   devise_parameter_sanitizer.for(:sign_up) { |u| u.permit({ roles: [] }, :email, :password,
+   devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit({ roles: [] }, :email, :password,
    :password_confirmation, :username) }
 
-  devise_parameter_sanitizer.for(:account_update) { |u| u.permit({ roles: [] }, :email, :password,
+  devise_parameter_sanitizer.permit(:account_update) { |u| u.permit({ roles: [] }, :email, :password,
   :password_confirmation, :current_password, :username) }
  end
 end
